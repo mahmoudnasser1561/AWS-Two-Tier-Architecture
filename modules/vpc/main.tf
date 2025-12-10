8940006b02b08bc21a6d94592ba73ad695a4ac1a
Mahmoud <mahmoudnasser1561@gmail.com>
2025-12-10 20:39:38 +0200

    adjust subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "two-tier-vpc" {
 
 resource "aws_subnet" "bastion_pub" {
   vpc_id            = aws_vpc.two-tier-vpc.id
-  cidr_block        = "10.0.7.0/24"
+  cidr_block        = "10.0.5.0/24"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
@@ -17,7 +17,7 @@ resource "aws_subnet" "bastion_pub" {
 
 resource "aws_subnet" "nat_pub" {
   vpc_id            = aws_vpc.two-tier-vpc.id
-  cidr_block        = "10.0.8.0/24"
+  cidr_block        = "10.0.6.0/24"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
