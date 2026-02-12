053607d4a3eeb1815b1ad1a2d0741efaee2e28b4
Mahmoud <mahmoudnasser1561@gmail.com>
2026-02-12 23:35:57 +0200

    fix(iam): replace GitHub OIDC AdministratorAccess with scoped least-privilege Terraform policy

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:mahmoud/AWS-Two-Tier-Architecture:*"  # Replace 'mahmoud/AWS-Two-Tier-Architecture' with your actual GitHub username/repo
+            "token.actions.githubusercontent.com:sub" = "repo:mahmoud/AWS-Two-Tier-Architecture:*" # Replace 'mahmoud/AWS-Two-Tier-Architecture' with your actual GitHub username/repo
           }
         }
       },
@@ -36,7 +36,129 @@ resource "aws_iam_role" "github_actions_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "github_actions_terraform_policy" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_role_policy" "github_actions_terraform_policy" {
+  name = "github-actions-terraform-policy"
+  role = aws_iam_role.github_actions_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ManageTwoTierInfraServices"
+        Effect = "Allow"
+        Action = [
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "autoscaling:*",
+          "rds:*",
+          "wafv2:*",
+          "cloudwatch:*",
+          "logs:*",
+          "sns:*",
+          "ssm:*"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "StateBucketAccess"
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::two-tier-tf-state-bucket-tf"
+      },
+      {
+        Sid    = "StateObjectAccess"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::two-tier-tf-state-bucket-tf/*"
+      },
+      {
+        Sid    = "AlbLogsBucketManagement"
+        Effect = "Allow"
+        Action = [
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:GetBucketAcl",
+          "s3:GetBucketPolicy",
+          "s3:GetBucketTagging",
+          "s3:ListBucket",
+          "s3:PutBucketPolicy",
+          "s3:PutBucketTagging",
+          "s3:DeleteBucketPolicy",
+          "s3:DeleteBucketTagging",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::two-tier-alb-logs-bucket",
+          "arn:aws:s3:::two-tier-alb-logs-bucket/*"
+        ]
+      },
+      {
+        Sid    = "StateLockTableAccess"
+        Effect = "Allow"
+        Action = [
+          "dynamodb:DescribeTable",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = "arn:aws:dynamodb:*:*:table/two-tier-tf-locks"
+      },
+      {
+        Sid    = "IamManagementForThisStack"
+        Effect = "Allow"
+        Action = [
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:GetRole",
+          "iam:UpdateAssumeRolePolicy",
+          "iam:TagRole",
+          "iam:UntagRole",
+          "iam:PutRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:GetRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+          "iam:CreateInstanceProfile",
+          "iam:DeleteInstanceProfile",
+          "iam:GetInstanceProfile",
+          "iam:AddRoleToInstanceProfile",
+          "iam:RemoveRoleFromInstanceProfile",
+          "iam:TagInstanceProfile",
+          "iam:UntagInstanceProfile",
+          "iam:ListInstanceProfilesForRole",
+          "iam:CreateOpenIDConnectProvider",
+          "iam:DeleteOpenIDConnectProvider",
+          "iam:GetOpenIDConnectProvider",
+          "iam:AddClientIDToOpenIDConnectProvider",
+          "iam:RemoveClientIDFromOpenIDConnectProvider",
+          "iam:UpdateOpenIDConnectProviderThumbprint",
+          "iam:ListOpenIDConnectProviders",
+          "iam:CreateServiceLinkedRole"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "PassEc2RolesOnly"
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = "arn:aws:iam::*:role/two-tier-*"
+        Condition = {
+          StringEquals = {
+            "iam:PassedToService" = "ec2.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
 }
